5b1c9da52036bb291b9d7f5945508d0582f01a66
riktimmondal <riktimrules@gmail.com>
2024-10-30 15:12:56 +0530

    Grpc Server Client in Go

---
+++ go_grpc/client/client_streaming.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	pb "go_grpc/proto"
+	"log"
+	"time"
+)
+
+func callSayHelloClientStream(client pb.GreetServiceClient, names *pb.NamesList) {
+	log.Printf("Client Streaming Started")
+	stream, err := client.SayHelloClientStreaming(context.Background())
+	if err != nil {
+		log.Fatalf("Could not send names: %v", err)
+	}
+	for _, name := range names.Names {
+		req := &pb.HelloRequest{
+			Name: name,
+		}
+		if err := stream.Send(req); err != nil {
+			log.Fatalf("Error while sending %v", err)
+		}
+		log.Printf("Sent the request with name: %s", name)
+		time.Sleep(2 * time.Second)
+	}
+
+	res, err := stream.CloseAndRecv()
+	log.Printf("Client streaming finished")
+	if err != nil {
+		log.Fatalf("Erro while receiving %v", err)
+	}
+	log.Printf("%v", res.Messages)
+}
