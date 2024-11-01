80ad63c5176c784a533934b380a39da6e3dde66e
riktimmondal <riktimrules@gmail.com>
2024-11-01 10:00:25 +0530

    golang postgres API

---
+++ go_postgre/go.mod
@@ -0,0 +1,8 @@
+module go-postgre
+
+go 1.22.4
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
+)
---
+++ go_postgre/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"go-postgre/router"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := router.Router()
+	fmt.Println("Starting server on port 8080...")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
